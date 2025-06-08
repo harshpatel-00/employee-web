2b79bf1c50d4fc7c2a7c16f4cc1c1436f8ac8e02
harshharaniya <158134844+harshharaniya@users.noreply.github.com>
2025-06-08 13:58:45 +0530

    Fix: Call init_db on startup

--- app.py
+++ app.py
@@ -134,5 +134,6 @@ def init_db():
     conn.close()
     
 if __name__ == '__main__':
+    init_db()
     port = int(os.environ.get("PORT", 5000))
     app.run(host = '0.0.0.0', port= port, debug=True)
